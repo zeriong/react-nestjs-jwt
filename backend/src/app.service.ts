009c612c370fe46c51af0802e27ebd43487f902e
zeriong <jaeryong95@gmail.com>
2022-12-07 21:03:15 +0900

    test commit

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private users: User[];
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  create(userData: CreateUserDTO) {
+    this.users.push({
+      id: this.users.length + 1,
+      ...userData,
+    });
   }
 }
