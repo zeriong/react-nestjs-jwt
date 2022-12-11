3fda258b0d1d2003df50cf2019c6a21c831e01c9
zeriong <jaeryong95@gmail.com>
2022-12-11 16:50:35 +0900

    백엔드 유저데이터 추가,삭제,수정,검색 완료

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,20 +1,45 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { CoreOutput } from '../../common/dtos/coreOutput.dto';
-import { CreateAccountDto } from './dtos/createAccount.dto';
 import { User } from '../../entities/user.entity';
+import { CoreOutput } from '../../common/dtos/coreOutput.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get()
-  getUsers(): Promise<User[]> {
-    return this.userService.getUsers();
+  /** 유저데이터 전체 검색 */
+  @Get('all')
+  getAll(): Promise<User[]> {
+    return this.userService.getAll();
   }
-
+  /** id로 유저데이터검색 */
+  @Get(':id')
+  getAcc(@Param('id') id: number) {
+    return this.userService.getAcc(id);
+  }
+  /** 유저데이터 생성 */
   @Post('resister')
-  async createAccount(@Body() input: CreateAccountDto): Promise<CoreOutput> {
+  createAccount(@Body() input) {
     return this.userService.createAccount(input);
+  }
+  /** id로 유저데이터 삭제 */
+  @Delete(':id')
+  delete(@Param('id') id: number): Promise<CoreOutput> {
+    return this.userService.delete(id);
+  }
+  /** 유저데이터 수정 */
+  @Patch(':id')
+  update(
+    @Param('id') id: number,
+    @Body() updateData: object,
+  ): Promise<CoreOutput> {
+    return this.userService.update(id, updateData);
   }
 }
